1dfb655c0279089ed65b59be7bf0c22343077f35
PSH <packtty18@naver.com>
2025-11-11 15:00:38 +0900

    Refactor : 중복 구문 리팩토링

--- Assets/02.Scripts/Item/Item.cs
+++ Assets/02.Scripts/Item/Item.cs
@@ -24,8 +24,6 @@ public class Item : MonoBehaviour
                 {
                     PlayerMove playerMove = collision.GetComponent<PlayerMove>();
                     playerMove.SpeedUp(Value);
-                    PlayerEffector playerEffector = collision.GetComponent<PlayerEffector>();
-                    playerEffector.InstantiateEffect(EItemType.MoveSpeedUp);
                     break;
                 }
 
@@ -33,18 +31,17 @@ public class Item : MonoBehaviour
                 {
                     PlayerFire playerFire = collision.GetComponent<PlayerFire>();
                     playerFire.SpeedUp(Value);
-                    PlayerEffector playerEffector = collision.GetComponent<PlayerEffector>();
-                    playerEffector.InstantiateEffect(EItemType.AttackSpeedUp);
                     break;
                 }
             case EItemType.HealthUp:
                 {
                     PlayerHealth player = collision.GetComponent<PlayerHealth>();
                     player.Heal(Value);
-                    PlayerEffector playerEffector = collision.GetComponent<PlayerEffector>();
-                    playerEffector.InstantiateEffect(EItemType.HealthUp);
                     break;
                 }
         }
+
+        PlayerEffector playerEffector = collision.GetComponent<PlayerEffector>();
+        playerEffector.InstantiateEffect(Type);
     }
 }
