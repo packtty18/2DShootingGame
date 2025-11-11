cb9919c69f86ff16bea297f100e112b0da47122c
PSH <packtty18@naver.com>
2025-11-11 13:15:46 +0900

    Feat : 배경 머티리얼 설정

---
+++ Assets/02.Scripts/Environment/BackgroundScroll.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class BackgroundScroll : MonoBehaviour
+{
+    public Material Material;
+    public float ScrollSpeed;
+
+    private void Update()
+    {
+        
+    }
+}
