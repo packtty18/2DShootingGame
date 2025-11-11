9d8787ab2a8224c78698567cbc3d6df096c3ad32
PSH <packtty18@naver.com>
2025-11-11 13:42:21 +0900

    Feat : 배경의 시차 스크롤링 적용

--- Assets/02.Scripts/Environment/BackgroundScroll.cs
+++ Assets/02.Scripts/Environment/BackgroundScroll.cs
@@ -2,11 +2,13 @@ using UnityEngine;
 
 public class BackgroundScroll : MonoBehaviour
 {
-    public Material Material;
-    public float ScrollSpeed;
+    public Material BackgroundMaterial;
+    public float ScrollSpeed = 0.1f;
 
     private void Update()
     {
-        
+        Vector2 direction = Vector2.up;
+
+        BackgroundMaterial.mainTextureOffset += direction * ScrollSpeed * Time.deltaTime;
     }
 }
