1ecb78652f906bfd8cb5fbb1845293e33c92a6bc
PSH <packtty18@naver.com>
2025-11-12 17:01:56 +0900

    Feat : 사운드 적용
    1. 플레이어 사격
    2. 적 사망시 폭발음
    3. 스페셜어택 폭발 + 유지 음
    4. 플레이어 피격
    5. 플레이어 사망(게임오버)
    6. 아이템 획득음

---
+++ Assets/02.Scripts/Etc/SoundObject.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class SoundObject : MonoBehaviour
+{
+     private AudioSource audio;
+    [SerializeField] private AudioClip clip;
+
+    private void Start()
+    {
+        audio = GetComponent<AudioSource>();
+        audio.clip = clip;
+        audio.Play();
+        Destroy(gameObject, clip.length);
+    }
+}
--- Assets/02.Scripts/Player/PlayerHealth.cs
+++ Assets/02.Scripts/Player/PlayerHealth.cs
@@ -1,25 +1,33 @@
-using System;
+﻿using System;
 using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
 {
     private PlayerStat _stat;
+    private PlayerEffector _effect;
 
     private float _health;
 
     private void Start()
     {
         _stat = GetComponent<PlayerStat>();
+        _effect = GetComponent<PlayerEffector>();
         _health = _stat.Health;
     }
 
     public void Hit(float damage)
     {
+        
         _health -= damage;
         if (_health <= 0)
         {
             Debug.Log("Player Dead");
+            _effect.InstantiateGameOverSoundObject();
             Destroy(gameObject);
+        }
+        else
+        {
+            _effect.PlayHitSound();
         }
     }
 
