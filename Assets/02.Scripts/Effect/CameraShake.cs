9b4b20061d2d1a18349d176b26f96b41c3054410
PSH <packtty18@naver.com>
2025-11-12 09:39:06 +0900

    Feat : 기능 자체는 완성

--- Assets/02.Scripts/Effect/CameraShake.cs
+++ Assets/02.Scripts/Effect/CameraShake.cs
@@ -1,16 +1,76 @@
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class CameraShake : MonoBehaviour
 {
     private Camera _camera; //대상 -> 메인카메라
     private Vector3 _initPosition = Vector3.zero;
+
     [SerializeField] private float _shakeTime =1; //흔들기 시간
-    [SerializeField] private Vector3 _shakeStrength; //흔드는 강도
-    [SerializeField] private float _shakeDelay; //카메라 이동과 이동 사이의 딜레이
+    [SerializeField] private Vector3 _shakeStrength = Vector3.zero; //흔드는 강도
+    [SerializeField] private float _shakeDelay = 0.1f; //카메라 이동과 이동 사이의 딜레이
+    [SerializeField] private AnimationCurve _shakeCurve = AnimationCurve.Linear(0, 1, 1, 0);
+
+    private float _time;
+    private float _delayTimer;
+
+    private bool _isPlaying;
 
     private void Start()
     {
         _camera = Camera.main;
         _initPosition = _camera.transform.position;
+        _delayTimer = _shakeDelay;
+    }
+
+    private void Update()
+    {
+        if(!_isPlaying)
+        {
+            return;
+        }
+
+        _time += Time.deltaTime;
+        _delayTimer -= Time.deltaTime;
+
+        
+
+        if( _delayTimer < 0 )
+        {
+            _delayTimer = _shakeDelay;
+
+            ShakeCamera(_time);
+        }
+    }
+
+    [ContextMenu("Shake")]
+    public void StartShake()
+    {
+        if(_isPlaying)
+        {
+            return;
+        }
+
+        _isPlaying = true;
+        _time = 0;
+        _delayTimer = _shakeDelay;
+    }
+
+    private void ShakeCamera(float time)
+    {
+        if (_time > _shakeTime)
+        {
+            EnnShake();
+            return;
+        }
+
+        Vector3 randomPosition = new Vector3(Random.Range(-0.1f, 0.1f), Random.Range(-0.1f, 0.1f), 0);
+        _camera.transform.position = _initPosition + randomPosition * _shakeCurve.Evaluate(time);
+    }
+
+    private void EnnShake()
+    {
+        _isPlaying = false;
+        _camera.transform.position = _initPosition;
     }
 }
