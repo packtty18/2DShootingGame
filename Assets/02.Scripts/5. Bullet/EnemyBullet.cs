6a8796c60bbc426f13c0a9f2b47a5a0bf9019ff9
PSH <packtty18@naver.com>
2025-11-14 13:56:50 +0900

    Feat : 적 총알 생성

---
+++ Assets/02.Scripts/5. Bullet/EnemyBullet.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class EnemyBullet : BulletBase
+{
+
+    protected override Vector2 GetNewPosition()
+    {
+        return (Vector2)transform.position + Vector2.down * _speed * Time.deltaTime;
+    }
+
+    protected override void OnTriggerEnter2D(Collider2D collision)
+    {
+        base.OnTriggerEnter2D(collision);
+        if (collision.TryGetComponent(out PlayerHealth health))
+        {
+            ApplyDamage(health);
+            OnHitTarget();
+        }
+    }
+}
