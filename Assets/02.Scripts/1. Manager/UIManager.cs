5763efce320d2d8c3ea090a9e7fdc8275fce6248
packtty18 <packtty18@naver.com>
2025-11-16 13:09:47 +0900

    Feat : 폭탄버튼, 데미지업 버튼 구현

--- Assets/02.Scripts/1. Manager/UIManager.cs
+++ Assets/02.Scripts/1. Manager/UIManager.cs
@@ -1,18 +1,45 @@
 ﻿using DG.Tweening;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+
+public enum EuttonType
+{
+    Attack,
+    Bomb,
+    DamageUp
+}   
 
 public class UIManager : SimpleSingleton<UIManager>
 {
     [SerializeField] private Text _highScoreTextUI;
     [SerializeField] private Text _scoreTextUI;
+    [SerializeField] private HealthUI _healthUI;
+    
 
     [SerializeField] private Vector3 _tweeningSize;
     [SerializeField] private float _tweenDuration;
 
     [Header("Input")]
-    public DynamicJoystick JoyStick;
-    public Button AttackButton;
+    [SerializeField] private DynamicJoystick _joyStick;
+    [SerializeField] private Button _attackButton;
+    [SerializeField] private Button _bombButton;
+    [SerializeField] private Button _damageUpButton;
+
+    private Dictionary<EuttonType, Button> _buttonDictionary = new Dictionary<EuttonType, Button>();
+
+    protected override void Awake()
+    {
+        base.Awake();
+        MatchButtonEnum();
+    }
+
+    private void MatchButtonEnum()
+    {
+        _buttonDictionary.Add(EuttonType.Attack, _attackButton);
+        _buttonDictionary.Add(EuttonType.Bomb, _bombButton);
+        _buttonDictionary.Add(EuttonType.DamageUp, _damageUpButton);
+    }
 
     private void ChangeText(Text target, string text)
     {
@@ -23,6 +50,23 @@ public class UIManager : SimpleSingleton<UIManager>
     {
         ChangeText(_highScoreTextUI, text);
     }
+
+    public void IncreaseHealthUI(int value)
+    {
+        for (int i = 0; i < value; i++)
+        {
+            _healthUI.IncreaseHealthUI();
+        }
+    }
+
+    public void DecreaseHealthUI(int value)
+    {
+        for (int i = 0; i < value; i++)
+        {
+            _healthUI.DecreaseHealthUI();
+        }
+    }
+    
 
     public void ChangeScoreText(string text, bool tween = true)
     {
@@ -40,4 +84,14 @@ public class UIManager : SimpleSingleton<UIManager>
         //Dotween을 사용한 방법
         //_scoreTextUI.transform.DOScale(_tweeningSize, _tweenDuration).SetLoops(2, LoopType.Yoyo);
     }
+
+    public DynamicJoystick GetJoystick()
+    {
+        return _joyStick;
+    }
+    public Button GetButton(EuttonType buttonType)
+    {
+        return _buttonDictionary[buttonType];
+    }
+
 }
